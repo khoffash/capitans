5cf36119848e660bbe4d32b62d7738d026f4d9e7
khoffash <amirkhoffashorg1@gmail.com>
2017-03-27 02:38:55 +0430

    Add files via upload

---
+++ plugins/echo.lua
@@ -0,0 +1,10 @@
+﻿local function run(msg, matches)
+if matches[1] == 'بگو' then
+local pext = matches[2]
+tdcli.sendMessage(msg.to.id, 0,1, pext,1,'html')
+end
+end
+
+return{ patterns= {'^(بگو) (.*)$'}, run=run}
+
+-- end by @khoffash
